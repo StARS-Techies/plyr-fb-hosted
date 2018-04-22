55fe745dbbbbcd5068ec583d91b875928dad551c
Beamanator <dabeamanator@gmail.com>
2018-04-22 17:10:43 +0200

    Update - V1.0.0

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,20 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const engines = require('consolidate');
+
 
 const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
 
-app.get('/route', (req, res) => {
-    res.send(`${Date.now()}`);
+app.get('/', (req, res) => {
+    console.log('here?');
+    // res.send(`${Date.now()}`);
+    res.render('index');
 });
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest(app);
+exports.myApp = functions.https.onRequest(app);
